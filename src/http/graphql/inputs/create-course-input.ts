c38be1e2ab49ba00944b00359fa617be20e10b0e
Felipe Ribeiro <piporibeiro@gmail.com>
2022-04-13 22:37:58 -0300

    feat: create classroom resolver and query

--- src/http/graphql/inputs/create-course-input.ts
+++ src/http/graphql/inputs/create-course-input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class CreateProductInput {
+export class CreateCourseInput {
   @Field()
   title: string;
 }
--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -4,11 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'path';
 import { DatabaseModule } from 'src/database/database.module';
-import { CustomersService } from 'src/services/customers.service';
-import { ProductsService } from 'src/services/products.service';
-import { PurchasesService } from 'src/services/purchases.service';
-import { ProductsResolver } from './graphql/resolvers/products.resolver';
-import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
+import { CoursesService } from '../services/courses.service';
+import { StudentsService } from '../services/students.service';
+import { EnrollmentsService } from '../services/enrollments.service';
 
 @Module({
   imports: [
@@ -20,11 +21,12 @@ import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
     }),
   ],
   providers: [
-    ProductsResolver,
-    ProductsService,
-    PurchasesResolver,
-    PurchasesService,
-    CustomersService,
+    CoursesResolver,
+    EnrollmentsResolver,
+    StudentsResolver,
+    CoursesService,
+    StudentsService,
+    EnrollmentsService,
   ],
 })
 export class HttpModule {}
